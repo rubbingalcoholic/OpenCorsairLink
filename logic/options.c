56a7b6b32403e52f73d213220604b83f93e18a2b
Audiohacked <audiohacked@gmail.com>
2019-06-28 14:22:24 -0600

    split options.h

--- logic/options.c
+++ logic/options.c
@@ -16,6 +16,8 @@
  * along with OpenCorsairLink.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+// #include "logic/support.h"
+// #include "logic/suboptions.h"
 #include "logic/options.h"
 
 #include "common.h"
